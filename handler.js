bee60d49ba7a90fecf9781bbc2cbff248f70af89
fachry2 <hckraza@gmail.com>
2016-06-24 12:15:46 +0700

    templating awal

--- handler.js
+++ handler.js
@@ -9,6 +9,27 @@ home = function(req, res){
 fb = function(req, res){
 	res.render('fb.html');
 };
+tekno=function(req, res){
+	res.render('tekno.html');
+};
+lowongan=function(req, res){
+	res.render('lowongan.html');
+};
+musik=function(req, res){
+	res.render('musik.html');
+};
+infomhs=function(req, res){
+	res.render('infomhs.html');
+};
+tips=function(req, res){
+	res.render('tips.html');
+};
+film=function(req, res){
+	res.render('film.html');
+};
+berita=function(req, res){
+	res.render('berita.html');
+};
 admin = function(req, res){
 	res.render('template/admin/index.html');
 }
@@ -18,7 +39,15 @@ admin = function(req, res){
 handler = {
 	home: home,
 	fb: fb,
-	admin:admin
+	admin:admin,
+	tekno:tekno,
+	lowongan:lowongan,
+	infomhs:infomhs,
+	musik:musik,
+	tips:tips,
+	film:film,
+	berita:berita
+
 	
 };
 
--- router.js
+++ router.js
@@ -6,6 +6,13 @@ var express = require('express'),
 router = function(app){
 	r.get('/', h.home);
 	r.get('/fb', h.fb);
+	r.get('/berita', h.berita);
+	r.get('/tips', h.tips);
+	r.get('/infomhs', h.infomhs);
+	r.get('/musik', h.musik);
+	r.get('/film', h.film);	
+	r.get('/tekno', h.tekno);
+	r.get('/lowongan', h.lowongan);
 	r.get('/admin',h.admin);
 	app.use(r);
 };
